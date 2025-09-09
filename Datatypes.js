bffd7f2487921349ffb834f612f411891feee857
Andrew Agahian <grayfoxx100@gmail.com>
2025-09-09 09:47:29 -0400

    9/8 lessons

---
+++ Datatypes.js
@@ -0,0 +1,17 @@
+var firstName = "Bryan"; // String
+var age = 25; // Number
+var isStudent = true; // Boolean
+var isFemale = false; // Boolean
+
+var middleName; // Undefined
+var lastName = null; // Null
+
+var person = {
+    'name': firstName,
+    'age': age,
+    'isStudent': isStudent,
+    'isFemale': isFemale,
+    'DOB':'01/01/1990'
+};   // Object
+
+console.log(person);
---
+++ app.js
@@ -0,0 +1,16 @@
+var name ="Andrew";
+
+let lastName ="Doozy";
+
+name ="William";
+lastName ="Smith";
+
+const fullName = "Andrew Doozy"; //const cannot be reassigned
+
+
+console.log(name);
+console.log(lastName);
+console.log(fullName);
+
+
+
