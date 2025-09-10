59e1e6c75dc4035a346372f2299561a66ade95f5
Andrew Agahian <grayfoxx100@gmail.com>
2025-09-10 14:44:29 -0400

    9/10 lessons

---
+++ functions.js
@@ -0,0 +1,26 @@
+var sum = add(2500, 4050599);
+var diff = sub(1000,80000);
+var val = sub(add(1,3), 2);
+var funct = sub (add(7,4), add(2,1));
+
+
+console.log(diff);
+
+console.log(sum);
+
+console.log(val);
+
+console.log(funct);
+
+
+function add(num1, num2) {
+    
+    return num1 + num2;
+
+}
+
+function sub(num1, num2) {
+    return num1 -num2;
+}
+
+
